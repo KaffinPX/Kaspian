f0c93baea279cd61b0ed4087a949f3624fe7530f
KaffinPX <kaffinpx@gmail.com>
2024-02-01 02:15:07 +0300

    Intro i18n

--- src/pages/CreateWallet/Intro.tsx
+++ src/pages/CreateWallet/Intro.tsx
@@ -8,7 +8,6 @@ import {
 import { useEffect, useState } from "react"
 import { clsx } from "clsx"
 import { Button } from "@/components/ui/button"
-import { useNavigate } from "react-router-dom"
 import Heading from "@/components/Heading"
 import {
   Carousel,
@@ -16,8 +15,11 @@ import {
   CarouselItem
 } from "@/components/ui/carousel"
 import { type CarouselApi } from "@/components/ui/carousel"
+import { i18n } from "webextension-polyfill"
 
-export default function Intro({ onConfirm }: { onConfirm: () => void }) {
+export default function Intro ({ onConfirm }: {
+  onConfirm: () => void 
+}) {
   const [ api, setApi ] = useState<CarouselApi>()
   const [ step, setStep ] = useState(1)
 
@@ -26,7 +28,7 @@ export default function Intro({ onConfirm }: { onConfirm: () => void }) {
 
     api.on("slidesInView", () => {
       const currentSlide = api.slidesInView()
-      console.log(`Step ${currentSlide[0]}`)
+
       setStep(currentSlide[0] + 1)
     })
   }, [ api ])
@@ -34,10 +36,8 @@ export default function Intro({ onConfirm }: { onConfirm: () => void }) {
   return (
     <main className={"flex flex-col justify-between min-h-screen py-6"}>
       <Heading
-        title={"Create Wallet"}
-        subtitle={
-          "This journey will be exciting, but you have to follow some rules."
-        }
+        title={i18n.getMessage('createWallet')}
+        subtitle={i18n.getMessage('rules')}
       />
       <div className={"px-4"}>
         {/* @ts-ignore */}
@@ -46,32 +46,22 @@ export default function Intro({ onConfirm }: { onConfirm: () => void }) {
             <CarouselItem>
               <div className={clsx("mx-auto px-5 text-center")}>
                 <DatabaseBackup className={"w-8 h-8 mx-auto text-green-400"} />
-                <h3 className={"text-xl"}>Back up your wallet</h3>
-                <p className={"text-sm"}>
-                  Your wallet is stored locally on your device. If you lose
-                  access to your device, or forget the password, you won't be
-                  able to recover your wallet without your mnemonic phrase.
-                </p>
+                <h3 className={"text-xl"}>{i18n.getMessage('backupWallet')}</h3>
+                <p className={"text-sm"}>{i18n.getMessage('selfCustody')}</p>
               </div>
             </CarouselItem>
             <CarouselItem>
               <div className={clsx("mx-auto px-5 text-center")}>
                 <MonitorOff className={"w-8 h-8 mx-auto text-green-400"} />
-                <h3 className={"text-xl"}>Never share your mnemonic phrase</h3>
-                <p className={"text-sm"}>
-                  Your mnemonic phrase is the key to your wallet. Anyone who has
-                  access to it can steal your funds.
-                </p>
+                <h3 className={"text-xl"}>{i18n.getMessage('shareMnemonic')}</h3>
+                <p className={"text-sm"}>{i18n.getMessage('walletAccess')}</p>
               </div>
             </CarouselItem>
             <CarouselItem>
               <div className={clsx("mx-auto px-5 text-center")}>
                 <BookUser className={"w-8 h-8 mx-auto text-green-400"} />
-                <h3 className={"text-xl"}>Verify the addresses you send to</h3>
-                <p className={"text-sm"}>
-                  Always verify the address you are sending to. If you send to
-                  the wrong address, your funds will be lost forever.
-                </p>
+                <h3 className={"text-xl"}>{i18n.getMessage('verifyAddresses')}</h3>
+                <p className={"text-sm"}>{i18n.getMessage('immutability')}</p>
               </div>
             </CarouselItem>
           </CarouselContent>
@@ -79,11 +69,11 @@ export default function Intro({ onConfirm }: { onConfirm: () => void }) {
       </div>
       <div className={"mx-auto"}>
         <Button onClick={() => {
-            if (step === 3) {
-              onConfirm()
-            }
-
-            api!.scrollNext()
+          if (step === 3) {
+            onConfirm()
+          }
+          
+          api!.scrollNext()
         }} className={"gap-2"}>
           {{
             1: <ChevronRight className={"w-6 h-6"} />,
@@ -91,9 +81,9 @@ export default function Intro({ onConfirm }: { onConfirm: () => void }) {
             3: <ArrowRightCircle className={"w-6 h-6"} />
           }[step]}
           {{
-            1: "Next",
-            2: "Next",
-            3: "Continue"
+            1: i18n.getMessage('next'),
+            2: i18n.getMessage('next'),
+            3: i18n.getMessage('continue')
           }[step]}
         </Button>
       </div>
