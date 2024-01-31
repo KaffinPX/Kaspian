1e430f0dfab734606ecff69359a8b02be82a1243
KaffinPX <kaffinpx@gmail.com>
2024-02-01 02:23:03 +0300

    Password i18n + message descriptions

--- src/pages/CreateWallet/Password.tsx
+++ src/pages/CreateWallet/Password.tsx
@@ -1,25 +1,21 @@
 import Heading from "@/components/Heading"
-import React from "react"
 import { Input } from "@/components/ui/input"
 import { Button } from "@/components/ui/button"
 import { ArrowRightCircle } from "lucide-react"
+import { i18n } from "webextension-polyfill"
 
-export default function Password({
-  onPasswordSet
-}: {
+export default function Password ({ onPasswordSet }: {
   onPasswordSet: () => void
 }) {
   return (
     <main className={"flex flex-col justify-between min-h-screen py-6"}>
       <Heading
-        title={"Set password"}
-        subtitle={
-          "Time to set a password. This password will be used to encrypt your wallet."
-        }
+        title={i18n.getMessage('setPassword')}
+        subtitle={i18n.getMessage('passwordDescription')}
       />
       <div className={"flex flex-col w-60 gap-3 mx-auto"}>
-        <Input type={"password"} placeholder={"Password"} />
-        <Input type={"password"} placeholder={"Confirm password"} />
+        <Input type={"password"} placeholder={i18n.getMessage('password')} />
+        <Input type={"password"} placeholder={i18n.getMessage('confirmPassword')} />
       </div>
       <div className={"mx-auto"}>
         <Button
@@ -30,7 +26,7 @@ export default function Password({
           className={"gap-2"}
         >
           <ArrowRightCircle />
-          Continue
+          {i18n.getMessage('confirm')}
         </Button>
       </div>
     </main>
