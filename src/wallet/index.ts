3684c1e9892fb586e2ca2838953f42b97d025c46
KaffinPX <kaffinpx@gmail.com>
2024-02-09 13:36:25 +0300

    Inherit initial node address from defaultSettings

--- src/wallet/index.ts
+++ src/wallet/index.ts
@@ -3,11 +3,12 @@ import Node from './controller/node'
 import RPC from './messaging'
 import Router from './messaging/router'
 
+import { defaultSettings } from '@/contexts/Settings'
 import load from "@/../wasm"
 
 load().then(() => {
   const wallet = new Wallet(() => {
-    const node = new Node('wss://kaspa.aspectron.com:443/mainnet')
+    const node = new Node(defaultSettings.nodes[0].address)
   
     new RPC(new Router(wallet, node))
   })
