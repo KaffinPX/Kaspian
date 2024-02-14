5bfe5bb950a0acac8006d5b84428eeede702b699
KaffinPX <kaffinpx@gmail.com>
2024-02-14 22:14:26 +0300

    UTXOCard => UTXOBoxes :pushpin:

--- src/pages/Wallet.tsx
+++ src/pages/Wallet.tsx
@@ -9,6 +9,7 @@ import useKaspa from "@/hooks/useKaspa"
 import { useEffect } from "react"
 import { Status } from "@/wallet/controller/wallet"
 import { useNavigate } from "react-router-dom"
+import { Input } from "@/components/ui/input"
 
 export default function Wallet () {
   const kaspa = useKaspa()
@@ -23,7 +24,7 @@ export default function Wallet () {
   }, [ kaspa.status ])
 
   return (
-    <main className={"flex flex-col justify-between min-h-screen py-6"}>
+    <main className={"flex flex-col justify-between min-h-screen py-6 gap-2"}>
       <div className={"flex flex-col gap-2"}>
         <div className={"flex flex-row justify-between items-center"}>
           <Heading title={"Kaspian"} />
@@ -31,18 +32,21 @@ export default function Wallet () {
             <SettingsSheet />
           </div>
         </div>
-
         <div className={"flex flex-col items-center"}>
           <p className={"text-4xl font-extrabold"}>100 KAS</p>
+          <p className={"text-xl font-bold"}>$ 0.00</p>
+          <Input
+            defaultValue={"kaspa:qpamkvhgh0kzx50gwvvp5xs8ktmqutcy3dfs9dc3w7lm9rq0zs76vf959mmrp"}
+            className={"text-s w-72"}
+            disabled={true}
+          />
         </div>
       </div>
 
-      <div className={"flex flex-col overflow-y-scroll no-scrollbar gap-2 h-80"}>
-        <UTXOCard
-          amount={100}
-          txId={"700c62a084a1b0559a10a70fcd5b9f3d9caca7d5075e5eec4cc9bf53c54b06c3"}
-          address={"kaspa:qrlksu3pxp6w7gxe8gw7lpajz8u64t5u958ucwveq49gxttaddetg8300l73p"}
-        />
+      <div className={"grid grid-cols-3 mx-4 h-fit <this-is-problematic-w-one-line overflow-y-scroll no-scrollbar gap-2"}>
+        <div className="bg-primary rounded-xl w-24 h-24 text-center">
+          <p className={"text-xl font-bold"}>100,000 KAS</p>
+        </div>
       </div>
       <div className={"flex flex-row justify-center gap-5"}>
         <SendDrawer />
