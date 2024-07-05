06126e5474e95a536fe956303fa8832a9b8e5dc1
KaffinPX <KaffinPX@gmail.com>
2024-07-05 04:09:23 +0300

    More dynamic window location calculation

--- src/wallet/messaging/wallet/provider/windows.ts
+++ src/wallet/messaging/wallet/provider/windows.ts
@@ -12,17 +12,29 @@ export default class Windows {
       .map(([key, value]) => `${encodeURIComponent(key)}=${encodeURIComponent(value)}`)
       .join('&')
 
+    const location = await this.calculateLocation()
     const { id } = await browser.windows.create({
       url: `./?${queryParams}#${hash}`,
+      top: location[0],
+      left: location[1],
       type: 'popup',
       width: 365,
-      height: 592,
+      height: 600, // TODO: find a way to remove this...
       focused: true
     })
 
     if (callback) this.windows.set(id!, callback)
 
     return id!
+  }
+
+  private async calculateLocation () {
+    const currentWindow = await browser.windows.getCurrent()
+    
+    return [
+      currentWindow.top! + 80,
+      currentWindow.left! + currentWindow.width! - 390
+    ]
   }
 
   private registerListener () {
