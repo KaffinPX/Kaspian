93e1bd5fc540bcace953b2306f2a027880c34ce7
KaffinPX <KaffinPX@gmail.com>
2024-07-05 17:07:05 +0300

    Remove a comment

--- src/provider/index.ts
+++ src/provider/index.ts
@@ -39,7 +39,6 @@ window.addEventListener('kaspa:connect', (event) => {
   window.addEventListener('kaspa:invoke', invokeListener)
 
   port.onDisconnect.addListener(() => {
-    // TODO: debug if port event listeners are getting clean
     window.removeEventListener('kaspa:invoke', invokeListener)
     window.dispatchEvent(new CustomEvent("kaspa:disconnect"))
   })
