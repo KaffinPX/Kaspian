72e3daef5b5a636ac95825a6ecb64af118b91455
KaffinPX <KaffinPX@gmail.com>
2024-10-12 03:22:08 +0300

    Clean-up shadcn and pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,6 @@
 import { MemoryRouter, Route, Routes } from 'react-router-dom'
 import { SettingsProvider } from './contexts/Settings'
 import { KaspaProvider } from './contexts/Kaspa'
-import Landing from './pages/Landing'
-import CreateWallet from '@/pages/CreateWallet'
-import Wallet from '@/pages/Wallet'
-import UnlockWallet from '@/pages/Unlock'
 
 function App () {
   return (
@@ -12,10 +8,6 @@ function App () {
       <KaspaProvider>
         <MemoryRouter>
           <Routes>
-            <Route path="/" element={<Landing />} />
-            <Route path="/create" element={<CreateWallet />} />
-            <Route path="/unlock" element={<UnlockWallet />} />
-            <Route path="/wallet" element={<Wallet />} />
           </Routes>
         </MemoryRouter>
       </KaspaProvider>
--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,6 @@
 import ReactDOM from 'react-dom/client'
 import App from './App'
 
-import './style.css'
-
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <App />
 )
