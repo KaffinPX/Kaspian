a6f04b408c7217c6d7d7104065f07b2012340692
KaffinPX <KaffinPX@gmail.com>
2024-10-13 17:29:09 +0300

    Initial scheme of Creation pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import { MemoryRouter, Route, Routes } from 'react-router-dom'
 import { SettingsProvider } from './contexts/Settings'
 import { KaspaProvider } from './contexts/Kaspa'
-
+import Landing from './pages/Landing'
 function App () {
   return (
     <SettingsProvider>
       <KaspaProvider>
         <MemoryRouter>
           <Routes>
+            <Route path="/" element={<Landing />} />
           </Routes>
         </MemoryRouter>
       </KaspaProvider>
---
+++ src/pages/Landing.tsx
@@ -0,0 +1,34 @@
+import { useEffect } from "react"
+import { useNavigate } from "react-router-dom"
+import useSettings from "../hooks/useSettings"
+import useKaspa from "../hooks/useKaspa"
+import usePromise from "../hooks/usePromise"
+import { Status } from "@/wallet/kaspa/wallet"
+
+export default function Landing() {
+  const settings = useSettings()
+  const { kaspa, load } = useKaspa()
+  const navigate = useNavigate()
+  
+  const [ loadedSettings ] = usePromise(() => {
+    return settings.load()
+  }, [])
+
+  const [ loadedKaspa ] = usePromise(() => {
+    return load()
+  }, [])
+
+  useEffect(() => {
+    if (loadedSettings && loadedKaspa) {
+      if (kaspa.status === Status.Uninitialized) {
+        navigate("/creation")
+      } else if (kaspa.status === Status.Locked) {
+        navigate("/:3")
+      } else if (kaspa.status === Status.Unlocked) {
+        navigate("/:3")
+      }
+    }
+  }, [ loadedSettings, loadedKaspa ])
+
+  return null
+}
